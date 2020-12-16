ff6f109b79084c43fdf99ffc675cb742cc62d511
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2020-12-16 20:47:43 +0200

    Update script_1.js

--- js/script_1.js
+++ js/script_1.js
@@ -1,2 +1,19 @@
-const ulCategoryRef = document.querySelector(ul#categories);
-console.log(ulCategoryRef)
+const ulCategoryRef = document.querySelector("#categories");
+const childrenNum = ulCategoryRef.children.length;
+console.log(`В списке ${childrenNum} категории`);
+
+const items = [...document.querySelectorAll("li.item")];
+items
+  .map((item) => {
+    const title = item.querySelector("h2").textContent;
+    const liNum = item.querySelectorAll("ul li").length;
+    return {
+      title,
+      liNum,
+    };
+  })
+  .forEach((entry) => {
+    console.log(
+      `Категория: ${entry.title}\nКоличество элементов: ${entry.liNum}`
+    );
+  });
