3a92764f89bb61bfc2b69defdc212e5f34c63b11
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2020-12-18 14:37:41 +0200

    Update script_task-6.js

--- js/script_task-6.js
+++ js/script_task-6.js
@@ -4,6 +4,8 @@ validInputRef.addEventListener('blur', handleInputBlur);
 
 function handleInputBlur (event) {
   if (event.target.value.length <= ###) {
-      
+      validInputRef.classlist.add('valid')
+      } else {
+        validInputRef.classlist.add('invalid')
       }
 }
