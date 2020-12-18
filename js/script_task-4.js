37bced2f0bb6793d0516fb2417ed10f50a1d68b5
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2020-12-18 10:31:49 +0200

    Update script_task-4.js

--- js/script_task-4.js
+++ js/script_task-4.js
@@ -9,11 +9,13 @@ let counterValue = 0;
 function increment () {
 counterValue += 1;
 valueRef.textContent = counterValue;
+  console.log("Increment +1")
 };
 
 function decrement () {
 counterValue -= 1;
 valueRef.textContent = counterValue;
+  console.log("Decrement -1")
 };
 
 incrementBtnRef.addEventListener ('click', increment);
