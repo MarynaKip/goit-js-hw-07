3e9d264bb09e6ca3eea0ab504fb033bf457c35ca
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2020-12-18 10:17:02 +0200

    Update script_task-4.js

--- js/script_task-4.js
+++ js/script_task-4.js
@@ -1,10 +1,20 @@
+const incrementBtnRef = document.querySelector('.counter button[data-action="increment"]');
+const decrementBtnRef = document.querySelector('.counter button[data-action="decrement"]');
+const valueRef = document.querySelector('#value');
+
+
+
 let counterValue = 0;
 
 function increment () {
 counterValue += 1;
+valueRef.textContent = counterValue;
 };
 
 function decrement () {
 counterValue -= 1;
+valueRef.textContent = counterValue;
 };
 
+incrementBtnRef.addEventListener ('click', increment);
+decrementBtnRef.addEventListener ('click', decrement)
