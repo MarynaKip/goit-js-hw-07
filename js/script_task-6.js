99590d447061e53d7562c5c9e472eae122e7ca31
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2020-12-18 14:41:39 +0200

    Update script_task-6.js

--- js/script_task-6.js
+++ js/script_task-6.js
@@ -3,7 +3,7 @@ const validInputRef = document.querySelector('#validation-input');
 validInputRef.addEventListener('blur', handleInputBlur);
 
 function handleInputBlur (event) {
-  if (event.target.value.length <= ###) {
+  if (event.target.value.length <= $(#validation-input).data('length')) {
       validInputRef.classlist.add('valid')
       } else {
         validInputRef.classlist.add('invalid')
