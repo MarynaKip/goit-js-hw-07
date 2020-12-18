950c21941c578d42cecfe67089e14b825648a698
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2020-12-18 14:11:14 +0200

    Update script_task-5.js

--- js/script_task-5.js
+++ js/script_task-5.js
@@ -3,5 +3,6 @@ const inputRef = document.querySelector('#name-input');
 inputRef.addEventListener('input', signChange);
 
 function signChange (event) {
-  inputRef.textContent = event.target.value
+  inputRef.textContent = event.target.value;
+    console.log("text added")
 }
