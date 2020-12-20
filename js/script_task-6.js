b83a18260b89a3831f1acb4332eefc11f3761388
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2020-12-20 22:52:40 +0200

    Update script_task-6.js

--- js/script_task-6.js
+++ js/script_task-6.js
@@ -3,14 +3,16 @@ const validInputRef = document.querySelector("#validation-input");
 validInputRef.addEventListener("blur", handleInputBlur);
 
 function handleInputBlur(event) {
-  console.log(document.getElementById("validation-input").value.length);
-  console.log(validInputRef.getAttribute("data-length"));
-  if (
-    Number(document.getElementById("validation-input").value.length) ===
-    Number(validInputRef.getAttribute("data-length"))
-  ) {
+  let validInputLength = document.getElementById("validation-input").value
+    .length;
+  let dataAttributeLength = validInputRef.getAttribute("data-length");
+  console.log(validInputLength);
+  console.log(dataAttributeLength);
+  if (Number(validInputLength) === Number(dataAttributeLength)) {
     validInputRef.classList.add("valid");
+    validInputRef.classList.remove("invalid");
   } else {
     validInputRef.classList.add("invalid");
+    validInputRef.classList.remove("valid");
   }
 }
