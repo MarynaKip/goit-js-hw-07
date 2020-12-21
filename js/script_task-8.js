2c332158e679d2e62ffe8ad61f3138b0d2c64c08
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2020-12-21 11:54:08 +0200

    Update script_task-8.js

--- js/script_task-8.js
+++ js/script_task-8.js
@@ -1,9 +1,14 @@
 const controlsRef = document.querySelector('#controls');
 const boxesRef = document.querySelector('#boxes');
 
+controlsRef.addEventListener('input', createBoxes);
 
+let addedElementsText = ""
 function createBoxes(amount) {
-  
+  let elementSize = 30;
+  for(let i=0, i<amount, i+=1) {
+    addedElementsText = addedElementsText + `<div ></div>`
+  }
 }
 
 function destroyBoxes() {
